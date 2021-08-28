0eb645272e6ea9de5452d3028ae92e8d1524af9e
StormKing969 <wijesinghesaj@gmail.com>
2021-08-28 01:47:12 -0400

    Importing data

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("hello")
+import requests
+import json
+
+data = requests.get("https://candidate.hubteam.com/candidateTest/v3/problem/dataset?userKey=09fc3b3c9df37bf824f90d470a4f")
+print(data)
