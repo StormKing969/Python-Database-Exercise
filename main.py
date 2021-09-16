0f94d45afc5fc1b0d241ba6541e5cfc5e50c8c8a
StormKing969 <wijesinghesaj@gmail.com>
2021-09-16 01:02:49 -0400

    Test

--- main.py
+++ main.py
@@ -2,4 +2,20 @@ import requests
 import json
 
 data = requests.get("https://candidate.hubteam.com/candidateTest/v3/problem/dataset?userKey=09fc3b3c9df37bf824f90d470a4f")
-print(data)
+data_set = data.json()
+
+if data.status_code == 404:
+    with open('dataset.json') as file:
+        data_set = json.load(file)
+
+new_dict = {}
+for key in data_set['events']:
+    # print(key)
+    if key['visitorId'] != new_dict:
+        new_dict['sessionByUser'] = [key['visitorId']]
+        for x in new_dict['sessionByUser']:
+            new_dict[x] = [key['url']]
+    else:
+        new_dict['sessionByUser'] = [key['visitorId']]
+
+print(new_dict)
